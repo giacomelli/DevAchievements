134e0500134256ba0c20fcfe227bcc17a54fdec9
Diego Giacomelli <giacomelli@gmail.com>
2013-11-10 18:00:42 -0200

    AppHarbor tests.

--- src/DevAchievements.Infrastructure.Web/Logging/Log4netLogStrategy.cs
+++ src/DevAchievements.Infrastructure.Web/Logging/Log4netLogStrategy.cs
@@ -4,6 +4,9 @@ using log4net;
 
 namespace DevAchievements.Infrastructure.Web.Logging
 {
+	/// <summary>
+	/// An ILogStrategy's implementation using Log4net.
+	/// </summary>
 	public class Log4netLogStrategy : LogStrategyBase
 	{
 		#region Fields
@@ -11,19 +14,33 @@ namespace DevAchievements.Infrastructure.Web.Logging
 		#endregion
 
 		#region implemented abstract members of LogStrategyBase
-
+		/// <summary>
+		/// Writes the debug log level message.
+		/// </summary>
+		/// <param name="message">Message.</param>
+		/// <param name="args">Arguments.</param>
 		public override void WriteDebug (string message, params object[] args)
 		{
 			s_logger.DebugFormat (message, args);
 			OnDebugWritten (new LogWrittenEventArgs (message, args));
 		}
 
+		/// <summary>
+		/// Writes the warning log level message.
+		/// </summary>
+		/// <param name="message">Message.</param>
+		/// <param name="args">Arguments.</param>
 		public override void WriteWarning (string message, params object[] args)
 		{
 			s_logger.WarnFormat (message, args);
 			OnWarningWritten (new LogWrittenEventArgs (message, args));
 		}
 
+		/// <summary>
+		/// Writes the error log level message.
+		/// </summary>
+		/// <param name="message">Message.</param>
+		/// <param name="args">Arguments.</param>
 		public override void WriteError (string message, params object[] args)
 		{
 			s_logger.ErrorFormat (message, args);
