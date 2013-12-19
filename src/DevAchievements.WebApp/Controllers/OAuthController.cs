ef739d5e49bb5b5a8c108b68c3ce0d75ab8967f9
Diego Giacomelli <giacomelli@gmail.com>
2013-12-18 22:21:13 -0200

    Web.config fix [ci skip]

--- src/DevAchievements.WebApp/Controllers/OAuthController.cs
+++ src/DevAchievements.WebApp/Controllers/OAuthController.cs
@@ -1,30 +1,32 @@
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Web;
-//using System.Web.Mvc;
-//using Microsoft.Web.WebPages.OAuth;
-//
-//namespace DevAchievements.WebApp.Controllers
-//{
-//    public class OAuthController : Controller
-//    {
-//        public ActionResult Index()
-//        {
-//            return View ();
-//        }
-//
-//		[HttpGet]
-//		public void RequestTwitter() 
-//		{
-//			OAuthWebSecurity.RequestAuthentication ("Twitter", "http://127.0.0.1:8080/OAuth/Twitter");
-//		}
-//
-//		[HttpGet]
-//		public ActionResult Twitter(string oauth_token) 
-//		{
-//			TempData ["token"] = oauth_token;
-//			return RedirectToAction("Create", "Developer");
-//		}
-//    }
-//}
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DotNetOpenAuth.AspNet.Clients;
+
+namespace DevAchievements.WebApp.Controllers
+{
+    public class OAuthController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View ();
+        }
+
+		[HttpGet]
+		public void RequestTwitter() 
+		{
+			var client = new TwitterClient("cjk1CorYK48VkRl7rAKCdg", "qxXoqjLsKNihQoIRZ7a5JPBpCTYNbFoSx5WzA9crY");
+
+			client.RequestAuthentication (HttpContext, new Uri("http://127.0.0.1:8080/OAuth/Twitter"));
+		}
+
+		[HttpGet]
+		public ActionResult Twitter(string oauth_token) 
+		{
+			TempData ["token"] = oauth_token;
+			return RedirectToAction("Create", "Developer");
+		}
+    }
+}
